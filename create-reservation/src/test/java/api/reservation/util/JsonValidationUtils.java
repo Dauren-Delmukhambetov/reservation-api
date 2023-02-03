bdd3d8013366217053bc2f6f0db59268635cdf10
Dauren Delmukhambetov <dauren.del@gmail.com>
2023-02-03 11:11:31 +0300

    Fixing test (#13)
    Signed-off-by: Dauren Delmukhambetov <dauren.del@gmail.com>

--- create-reservation/src/test/java/api/reservation/util/JsonValidationUtils.java
+++ create-reservation/src/test/java/api/reservation/util/JsonValidationUtils.java
@@ -6,7 +6,7 @@ public class JsonValidationUtils {
 
     public static String ofTodayDateInProperty(String property) {
         final var today = OffsetDateTime.now();
-        final var datePattern = String.format("%d-%02d-%dT\\d{2}:\\d{2}:\\d{2}\\.\\d{1,6}((\\+\\d{2}:\\d{2})|(Z))", today.getYear(), today.getMonthValue(), today.getDayOfMonth());
+        final var datePattern = String.format("%d-%02d-%02dT\\d{2}:\\d{2}:\\d{2}\\.\\d{1,6}((\\+\\d{2}:\\d{2})|(Z))", today.getYear(), today.getMonthValue(), today.getDayOfMonth());
         return String.format("\"%s\": \"%s\"", property, datePattern);
     }
 
